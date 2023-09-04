87d448d376f221e3f6f386e59bf6d736c0a541ee
ismail-dias <dismtzking@gmail.com>
2023-09-04 16:47:06 +0700

    [UPDATE] update trigger.py

--- trigger.py
+++ trigger.py
@@ -1,14 +1,10 @@
-from ubidots import ApiClient
+import requests
 import RPi.GPIO as GPIO
 import time
 
-
-# Konfigurasi Ubidots
 UBIDOTS_TOKEN = "BBFF-03MEmiXujs99bPymSwqiVXcNo5t545"
 DEVICE_LABEL = "raspberry-pi-motor"
-
-api = ApiClient(token="BBFF-03MEmiXujs99bPymSwqiVXcNo5t545")
-variable = api.get_variable("64e36cbbf9da915170e8fb71")
+VARIABLE_LABEL_1 = "motor-command"
 
 # Konfigurasi GPIO
 GPIO.setmode(GPIO.BCM)
@@ -16,15 +12,25 @@ pin_relay = 18
 GPIO.setup(pin_relay, GPIO.OUT)
 GPIO.setwarnings(False)
 
+# def build_payload(relay):
+#     payload = {
+#         VARIABLE_LABEL_1:relay}
+#     return payload
+
 try:
     while True:
-        last_value = variable.get_values(1)
-        if last_value[0].get("value") == 1:
+        url = "http://industrial.api.ubidots.com"
+        url = "{}/api/v1.6/devices/{}/{}/lv".format(url, DEVICE_LABEL, VARIABLE_LABEL_1)
+        params = {"page_size":1}
+        headers = {"X-Auth-Token": UBIDOTS_TOKEN}
+        #try:
+        req = requests.get(url, headers=headers, params=params)
+        print(req)
+        last_values = req.json()
+        if last_values == 1:
             GPIO.output(pin_relay, GPIO.LOW)
-            time.sleep(0.1)
         else:
             GPIO.output(pin_relay, GPIO.HIGH)
-            time.sleep(0.1)
-            
 except KeyboardInterrupt:
     GPIO.cleanup()
+
